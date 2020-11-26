c8fd384815f476707a4b1607613d4cf63baec37b
WYseven <consolelog@126.com>
2020-11-26 08:12:28 +0800

    增加全排列2-有重复项

--- 回溯/46.全排列.js
+++ 回溯/46.全排列.js
@@ -4,12 +4,25 @@
  */
 var permute = function(nums) {
 
-  var loop = (arr) => {
-    if(arr.length === 1) return arr
+  let result = []  // 存放总结果
+  let child = [] // 存放每组遍历的字结果
+
+  var loop = (nums) => {
+    if(child.length === nums.length) {
+      result.push([...child])
+      return 
+    }
+    for(let i = 0; i < nums.length; i++){
+      if(child.includes(nums[i])) continue
+      child.push(nums[i])
+      loop(nums)
+      child.pop()
+    }
     
   }
   loop(nums)
+  return result
 };
 
 
-permute([1,2,3])
+console.log(permute([1,2,3]))
---
+++ 回溯/47.全排列二.js
@@ -0,0 +1,42 @@
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+// nums 中包含重复的项
+var permute = function(nums) {
+
+  let result = []  // 存放总结果
+  let vis = [] // 存放重复的已经填过的数字的标识, [true,true,false] 这样
+
+  var loop = (nums, inx, child) => {
+
+    if(inx === nums.length) {
+      result.push([...child])
+      return
+    }
+    
+    for(let i = 0; i < nums.length; i++){
+
+      if(vis[i]) continue  // 已经存在
+      if( i > 0 && nums[i] === nums[i - 1] && !vis[i - 1] ){
+        continue
+      }
+
+      vis[i] = true
+      child.push(nums[i])
+      loop(nums, inx + 1, child)
+      child.pop();
+      vis[i] = false
+
+    }
+  
+  }
+  // 先排序，保证重复的数字挨着
+  nums = nums.sort((a, b) => a - b)
+  loop(nums, 0, [])
+  return result
+};
+
+
+console.log(permute([1,2,3]))
+console.log(permute([1,1,1, 3]))
